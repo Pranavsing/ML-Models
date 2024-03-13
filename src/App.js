fcec069cc46d1ee5b09611c2cb15d42ad8cd22dd
Vishisht Dubey <98794736+vishisht-dubey@users.noreply.github.com>
2024-03-14 00:30:12 +0530

    APIs Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // App.js
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import animationData from "./animation/ani.json";
-
+import axios from "axios";
 import ModelList from "./ModelList";
 import FeaturedList from "./FeaturedList";
 import ModelDetails from "./ModelDetails";
@@ -11,32 +11,32 @@ import Generate from "./Generate";
 import "./App.css";
 
 const App = () => {
-	const [models] = useState([
-		{ id: 1, name: "Image-to-Text Model", category: "Computer Vision" },
-		{
-			id: 2,
-			name: "Language Translation Model",
-			category: "Natural Language Processing",
-		},
-		{
-			id: 3,
-			name: "Image Generator Model",
-			category: "Image Processing Processing",
-		},
-		// Add more models...
-	]);
-
-	const [featuredModels] = useState([
-		{ id: 1, name: "Image-to-Text Model", reason: "Most viewed" },
-		// Add more featured models...
-	]);
-
+	const [models, setModels] = useState([]);
+	const [featuredModels, setFeaturedModels] = useState([]);
+	const getModels = async () => {
+		const response = await axios.get("http://localhost:4000/models");
+		if (response.status == 200) {
+			setModels(response.data);
+		}
+		console.log(response);
+	};
+	const getFeaturedModels = async () => {
+		const response = await axios.get("http://localhost:4000/featuredModels");
+		if (response.status === 200) {
+			setFeaturedModels(response.data);
+		}
+		console.log(response);
+	};
 	const [selectedModel, setSelectedModel] = useState(null);
 
 	const handleModelClick = (model) => {
 		setSelectedModel(model);
 	};
-
+	useEffect(() => {
+		getModels();
+		getFeaturedModels();
+	}, []);
+	console.log(featuredModels,models)
 	const scrollRef = useRef(null);
 
 	const scrollToSection = (sectionId) => {
@@ -82,7 +82,11 @@ const App = () => {
 						element={
 							<React.Fragment>
 								<div className="head">
-									<Lottie animationData={animationData} className="animation" style={{width:"50%"}} />
+									<Lottie
+										animationData={animationData}
+										className="animation"
+										style={{ width: "50%" }}
+									/>
 									<div style={{ width: "50%" }}>
 										<p style={{ fontSize: "102px", fontWeight: "1000" }}>
 											AI Models
@@ -93,13 +97,21 @@ const App = () => {
 										</p>
 									</div>
 								</div>
-								<FeaturedList
-									featuredModels={featuredModels}
-									onModelClick={handleModelClick}
-								/>
-								<div id="browse" style={{ width: "100%", marginTop: "20px" }}>
-									<ModelList models={models} onModelClick={handleModelClick} />
-								</div>
+								{featuredModels.length > 0 && (
+									<FeaturedList
+										featuredModels={featuredModels}
+										onModelClick={handleModelClick}
+									/>
+								)}
+								{models.length > 0 && (
+									<div id="browse" style={{ width: "100%", marginTop: "20px" }}>
+										<ModelList
+											models={models}
+											onModelClick={handleModelClick}
+										/>
+									</div>
+								)}
+
 								{selectedModel && (
 									<ModelDetails selectedModel={selectedModel} />
 								)}
---
+++ text-backend/data.js
@@ -0,0 +1,20 @@
+const models = [
+    { id: 1, name: "Image-to-Text Model", category: "Computer Vision" },
+    {
+        id: 2,
+        name: "Language Translation Model",
+        category: "Natural Language Processing",
+    },
+    {
+        id: 3,
+        name: "Image Generator Model",
+        category: "Image Processing Processing",
+    },
+    // Add more models...
+];
+
+const featuredModels = [
+    { id: 1, name: "Image-to-Text Model", reason: "Most viewed" },
+    // Add more featured models...
+];
+module.exports = {models, featuredModels}
